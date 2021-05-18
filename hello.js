b094b29d6f926c924985c64d17db43b5e89ba065
dlwndrhs05 <dlwndrhs05@gmail.com>
2021-05-18 16:08:45 +0900

    enter

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+
+let date = new Date()
+if (date.getHours()<12){
+    console.log("오전입니다.");
+}
+else
+console.log("오후입니다.")
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getTime());
+console.log(date.getUTCMinutes());
